7d4a7aaedc476e3e89bd6fd47cc42b235159717e
Unknown <matt_cooper1@hotmail.com>
2018-04-10 15:33:07 +0100

    Blocks are spawning in square grid, models set up, base classes setup

---
+++ IntegratedGroupProject/Assets/Scripts/AnswerBlock.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerBlock : Block, ISelectable
+{
+    public SpriteRenderer sprite;
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    public string GetEquation()
+    {
+        return equation;
+    }
+    public string GetAnswer()
+    {
+        return answer;
+    }
+    private void SetEquation()
+    {
+        equation = EquationModel.instance.GetRandomEquation();
+    }
+    private void SetAnswer()
+    {
+        equation = AnswerModel.instance.GetRandomAnswer();
+    }
+    
+    public void Deselect()
+    {
+        sprite.color = Color.white;
+        gameObject.transform.localScale += new Vector3(-0.5f, -0.5f, 0);
+    }
+
+    public GameObject GetGameObject()
+    {
+        return gameObject;
+    }
+
+    public void Select()
+    {
+        sprite.color = Color.green;
+        gameObject.transform.localScale += new Vector3(0.5f, 0.5f, 0);
+    }
+}
---
+++ IntegratedGroupProject/Assets/Scripts/EquationModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquationModel : MonoBehaviour {
+
+    public string[] EquationArray;
+    public static EquationModel instance { get; private set; }
+
+    private void Start()
+    {
+        instance = this;
+    }
+    public string GetRandomEquation()
+    {
+        return EquationArray[Random.Range(0, EquationArray.Length)];
+    }
+    public void SetupEquationArray(int gridsize)
+    {
+        switch (gridsize)
+        {
+            case 4:
+                EquationArray = new string[] { "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation" };
+        
+                break;
+            case 6:
+                EquationArray = new string[] { "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation"};
+                break;
+            case 8:
+                EquationArray = new string[] { "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation",
+                                               "Equation", "Equation", "Equation", "Equation" };
+                break;
+            default: break;
+        }
+    }
+}
